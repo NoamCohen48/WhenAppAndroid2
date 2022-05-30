76a7e907eeb7b276f0dff5b99636a9b7febc9cac
shakedc1599 <47411973+shakedc1599@users.noreply.github.com>
2022-05-30 15:46:04 +0300

    ChatScreen

--- app/src/main/java/com/example/whenappandroid/ChatScreen.java
+++ app/src/main/java/com/example/whenappandroid/ChatScreen.java
@@ -1,5 +1,6 @@
 package com.example.whenappandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -8,13 +9,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import com.example.whenappandroid.Data.Contact;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChatScreen extends AppCompatActivity {
 
     private AppDB db;
     private ContactDao contactDao;
+    ArrayAdapter<Contact> adapter;
+    List<Contact> contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +32,33 @@ public class ChatScreen extends AppCompatActivity {
 
         contactDao = db.contactDao();
 
+        FloatingActionButton btnAdd = findViewById(R.id.btnAddContact);
+        btnAdd.setOnClickListener(v -> {
+            Intent i = new Intent(this, AddContactFragment.class);
+            startActivity(i);
+        });
+
         Contact shaked = new Contact(0, "shaked", "a", "hey", "");
         Contact noam = new Contact(0, "noam", "a", "hi", "");
 
         contactDao.insert(shaked, noam);
 
         ListView lvContacts = findViewById(R.id.lvContacts);
-        List<Contact> contacts = contactDao.index();
+        contacts = new ArrayList<>();
 
-        ArrayAdapter<Contact> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, contacts);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, contacts);
 
         lvContacts.setAdapter(adapter);
 
+    }
 
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        contacts.clear();
+        contacts.addAll(contactDao.index());
+        adapter.notifyDataSetChanged();
     }
 
 }
