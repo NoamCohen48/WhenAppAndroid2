6028a0a713e4e2a7e81564dee08a270891c4936e
Roi Avraham <roicvc5@gmail.com>
2022-05-30 17:12:31 +0300

    sfdsdf

---
+++ app/src/main/java/com/example/whenappandroid/SettingsScreen/SettingsActivity.java
@@ -0,0 +1,38 @@
+package com.example.whenappandroid.SettingsScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+
+import com.example.whenappandroid.R;
+import com.example.whenappandroid.databinding.ActivityChatScreenBinding;
+import com.example.whenappandroid.databinding.ActivitySettingsBinding;
+
+public class SettingsActivity extends AppCompatActivity {
+    private ActivitySettingsBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding  = ActivitySettingsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.submitBtn.setOnClickListener(v -> {
+            SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("server", binding.serverInputBox.getText().toString());
+            editor.apply();
+        });
+
+        binding.themeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("dark", isChecked);
+            editor.apply();
+        });
+    }
+}
