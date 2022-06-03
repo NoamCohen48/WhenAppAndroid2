9f13eb299828596275a4ad689c12465758ece370
Noam Cohen <92931230+NoamCohen48@users.noreply.github.com>
2022-06-03 19:00:30 +0300

    starteasdasdd

--- app/src/main/java/com/example/whenappandroid/Login/LoginActivity.java
+++ app/src/main/java/com/example/whenappandroid/Login/LoginActivity.java
@@ -1,18 +1,24 @@
 package com.example.whenappandroid.Login;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-import com.example.whenappandroid.R;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.whenappandroid.ChatScreen.Vertical.VerticalContactsActivity;
 import com.example.whenappandroid.Register.RegisterActivity;
 import com.example.whenappandroid.databinding.ActivityLoginBinding;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class LoginActivity extends AppCompatActivity {
+
     private ActivityLoginBinding binding;
+    private LoginViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +27,33 @@ public class LoginActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        binding.btnToRegister.setOnClickListener(v -> {
+        viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+
+        binding.signinBtn.setOnClickListener(v -> {
             Intent i = new Intent(this, RegisterActivity.class);
             startActivity(i);
+        });
+
+        binding.loginBtn.setOnClickListener(v -> {
+            String username = binding.usernameInputBox.getText().toString();
+            String password = binding.passwordInputBox.getText().toString();
+
+            viewModel.login(username, password).enqueue(new Callback<String>() {
+                @Override
+                public void onResponse(Call<String> call, Response<String> response) {
+                    if (response.body() == null) {
+                        binding.ErrorText.setText("Combination is incorrect");
+                    }
+
+                    Intent intent = new Intent(LoginActivity.this, VerticalContactsActivity.class);
+                    startActivity(intent);
+                }
+
+                @Override
+                public void onFailure(Call<String> call, Throwable t) {
+                    binding.ErrorText.setText("Error connecting to server");
+                }
+            });
         });
     }
 
