d40e2400e53cda93c4433d437cd50e86f16bcdba
Noam Cohen <92931230+NoamCohen48@users.noreply.github.com>
2022-06-04 16:02:41 +0300

    xvxvcvxcv

--- app/src/main/java/com/example/whenappandroid/ChatScreen/AddContactActivity.java
+++ app/src/main/java/com/example/whenappandroid/ChatScreen/AddContactActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.example.whenappandroid.Login.LoginViewModel;
 import com.example.whenappandroid.databinding.ActivityAddContactBinding;
 
 public class AddContactActivity extends AppCompatActivity {
     private ActivityAddContactBinding binding;
+    private ContactViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,23 @@ public class AddContactActivity extends AppCompatActivity {
         binding = ActivityAddContactBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        viewModel = new ViewModelProvider(this).get(ContactViewModel.class);
+
+        binding.addBtn.setOnClickListener(v -> {
+            String username = binding.usernameInputBox.getText().toString();
+            String nickname = binding.nicknameInputBox.getText().toString();
+            String server = binding.serverInputBox.getText().toString();
+
+            String added = viewModel.insert(username, nickname, server);
+
+            if (added == null) {
+                finish();
+            }
+
+            binding.errorText.setText(added);
+        });
+
+        binding.cancelBtn.setOnClickListener(v -> finish());
     }
 }
