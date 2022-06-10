1864554c95e69549390f56d61a772908cf63ca33
Roi Avraham <roicvc5@gmail.com>
2022-06-10 10:34:52 +0300

    khbgkh

--- app/src/main/java/com/example/whenappandroid/ChatScreen/Vertical/VerticalMessagesActivity.java
+++ app/src/main/java/com/example/whenappandroid/ChatScreen/Vertical/VerticalMessagesActivity.java
@@ -1,7 +1,11 @@
 package com.example.whenappandroid.ChatScreen.Vertical;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.TextView;
