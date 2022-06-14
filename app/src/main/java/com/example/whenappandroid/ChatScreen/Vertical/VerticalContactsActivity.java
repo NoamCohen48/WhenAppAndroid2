ad0fd5d381364a6e6bdc76d2691091c5e27be6cf
Noam Cohen <92931230+NoamCohen48@users.noreply.github.com>
2022-06-14 22:15:59 +0300

    asdadasd

--- app/src/main/java/com/example/whenappandroid/ChatScreen/Vertical/VerticalContactsActivity.java
+++ app/src/main/java/com/example/whenappandroid/ChatScreen/Vertical/VerticalContactsActivity.java
@@ -3,6 +3,7 @@ package com.example.whenappandroid.ChatScreen.Vertical;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,11 +19,19 @@ import com.example.whenappandroid.ChatScreen.ContactList.ContactListAdapter;
 import com.example.whenappandroid.ChatScreen.ContactList.ContactViewModel;
 import com.example.whenappandroid.ChatScreen.Horizontal.ChatHorizontalActivity;
 import com.example.whenappandroid.Data.Contact;
+import com.example.whenappandroid.Data.Globals;
+import com.example.whenappandroid.Data.RetrofitService;
+import com.example.whenappandroid.Data.ServerAPI;
+import com.example.whenappandroid.FirebaseService;
 import com.example.whenappandroid.R;
 import com.example.whenappandroid.SettingsScreen.SettingsActivity;
 import com.example.whenappandroid.databinding.ActivityVerticalContactsBinding;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class VerticalContactsActivity extends AppCompatActivity {
     private ActivityVerticalContactsBinding binding;
@@ -42,6 +51,13 @@ public class VerticalContactsActivity extends AppCompatActivity {
             intent.putExtra("contact", contact);
             startActivity(intent);
         });
+
+        String token = FirebaseService.getToken(this);
+        sendRegistrationToServer(token);
+
+//        Intent serviceIntent = new Intent();
+////        serviceIntent.setAction("com.google.firebase.MESSAGING_EVENT");
+//        startService(serviceIntent);
 
         ContactViewModel contactViewModel = new ViewModelProvider(this).get(ContactViewModel.class);
         contactViewModel.getAllContacts().observe(this, (List<Contact> list) -> {
@@ -77,5 +93,20 @@ public class VerticalContactsActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
         return super.onOptionsItemSelected(item);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        ServerAPI api = RetrofitService.getAPI(Globals.getServerAndroid());
+        api.registerToken(new ServerAPI.RegisterTokenPayload(Globals.currentUser, token)).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Log.d("custom:", "sendRegistrationToServer: " + token);
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                int x;
+            }
+        });
     }
 }
